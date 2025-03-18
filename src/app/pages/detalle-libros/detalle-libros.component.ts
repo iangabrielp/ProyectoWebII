b8783bc553f3a0d7a3ca7c2bb3eb59778d68bdcb
CrasherG4 <cgp4762@gmail.com>
2025-03-17 19:32:08 -0500

    avance

--- src/app/pages/detalle-libros/detalle-libros.component.ts
+++ src/app/pages/detalle-libros/detalle-libros.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { LibrosService } from '../../services/libros.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AutorService } from '../../services/autor.service';
 
 @Component({
   selector: 'app-detalle-libros',
@@ -11,30 +12,27 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrl: './detalle-libros.component.css'
 })
 export class DetalleLibrosComponent {
-  constructor(private servicio: LibrosService, private ruta: ActivatedRoute, private router: Router){}
+  constructor(private servicio: LibrosService, private ruta: ActivatedRoute, private router: Router, private servicioAutor:AutorService){}
 
-  id:any;
-  nombre:any;
-  autor:any;
-  stock:any;
-  img:any;
-  item:any;
+  libro:any={
+    id:null,
+    titulo:'',
+    genero:'',
+    fechapublicacion:'',
+    img:'',
+    autor:null,
+  }
+  autores:any[]=[]
 
   editar(formulario:any):void{
     this.servicio.putLibro(formulario.value).subscribe(()=>{
       this.router.navigate(['/libros'])
     })
+    
   }
   ngOnInit(){
-    this.ruta.params.subscribe(u => {
-      this.servicio.getLibroById(u["idLibro"]).subscribe(libro=>{
-        this.item=libro
-        this.id=this.item.id
-        this.nombre=this.item.nombre
-        this.autor=this.item.autor
-        this.stock=this.item.stock
-        this.img=this.item.img
-      })
+    this.servicioAutor.getAutores().subscribe(autor=>{
+      this.autores=autor;
     })
   }
 }
